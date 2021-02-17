5632a7288af2c3818a18c2fb9e2f7353de80483d
SAANVI1976 <73863642+SAANVI1976@users.noreply.github.com>
2021-02-17 13:15:07 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,13 @@
+canvas = document.getElementById("mycanvas");
+
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+
+ctx.strokeStyle = "grey";
+
+ctx.lineWidth = 4;
+
+ctx.rect(150, 143, 430, 200);
+
+ctx.strokeStyle();
